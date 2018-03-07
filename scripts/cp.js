823208d291b40ed4dba6daeaf74a679858bf9bdd
pingan1927 <pengsencai1986@gmail.com>
2018-03-07 14:10:15 +0800

    add scripts

---
+++ scripts/cp.js
@@ -0,0 +1,8 @@
+const fs = require('fs-extra')
+const path = require('path')
+// copy file
+fs.copySync(path.join(__dirname, '../src'), path.join(__dirname, '../es'), {
+  filter(src, des){
+    return !src.endsWith('.js');
+  }
+});
