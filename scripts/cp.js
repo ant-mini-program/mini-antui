a0537fd6e6af577fbbba6430a0f3443f8dc20bdb
pingan1927 <pengsencai1986@gmail.com>
2018-03-08 00:10:34 +0800

    clear

--- scripts/cp.js
+++ scripts/cp.js
@@ -1,5 +1,5 @@
-const fs = require('fs-extra')
-const path = require('path')
+const fs = require('fs-extra');
+const path = require('path');
 // copy file
 fs.copySync(path.join(__dirname, '../src'), path.join(__dirname, '../es'), {
   filter(src, des){
---
+++ scripts/rm.js
@@ -0,0 +1,20 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+// remove unnecessary file
+const dirs = fs.readdirSync(path.join(__dirname, '../es'));
+
+dirs.forEach((item) => {
+  if (item.includes('app.') || item.includes('DS_Store')) {
+    fs.removeSync(path.join(__dirname, '../es/', item));
+  } else {
+    const moduleDirs = fs.readdirSync(path.join(__dirname, '../es/', item));
+    moduleDirs.forEach((item2) => {
+      if (item2.includes('demo')) {
+        fs.removeSync(path.join(__dirname, '../es/', item, item2));
+      }
+    });
+  }
+});
+
+fs.removeSync(path.join(__dirname, '../lib/'));
