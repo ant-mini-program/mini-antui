9675559228643f1419e33c935447c870dd2e0d6e
roninliu <roninliu@foxmail.com>
2018-04-08 17:43:49 +0800

    fix the code problem and add click event check

--- src/notice/index.js
+++ src/notice/index.js
@@ -5,11 +5,11 @@ Component({
   props: {
     mode: "", //closable,link
     action: "", //文本按钮
-    show:true,//是否显示
+    show: true, //是否显示
   },
   methods: {
     onTap() {
-      if(this.props.onClick){
+      if (this.props.onClick) {
         this.props.onClick()
       }
     }
