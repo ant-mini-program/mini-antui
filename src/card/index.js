798f0dd0fbf6b3078759183800c9b4d1fa1eb385
pingan1927 <pengsencai1986@gmail.com>
2018-04-16 14:01:59 +0800

    add card

--- src/card/index.js
+++ src/card/index.js
@@ -1,28 +1,13 @@
 Component({
-  data: {
-    title22: 'test',
-    subTitle22: 'test',
-  },
   props: {
-    title: '卡片标题22',
-    subTitle: '副标题非必填22',
-  },
-  onReady() {
-    console.log(this.props, 'dada');
-    this.setData({
-      title: this.props.title,
-      subTitle: this.props.subTitle,
-    });
-  },
-  onUpdate(prevProps,prevData){
-
-  },
-  onUnload(){
-
+    title: '',
+    onClick: () => {},
+    info: '',
   },
   methods:{
-    onCardClick(ev){
-      this.props.onCardClick({ ...ev, e2:1});
+    onCardClick() {
+      const { info, onClick } = this.props;
+      onClick({info});
     },
   },
 })
