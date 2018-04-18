05825de0e67403c89e7d7fea07c9f362515aabfc
Phieo <shikong2014@gmail.com>
2018-04-18 13:56:45 +0800

    remove console log

--- src/search-bar/demo/index.js
+++ src/search-bar/demo/index.js
@@ -3,33 +3,25 @@ Page({
     value: '美食',
   },
   handleInput(value) {
-    console.log(value, 'onInput');
     this.setData({
       value,
     });
   },
   handleClear(value) {
-    console.log(value, 'onClear');
     this.setData({
       value: '',
     });
   },
-  handleFocus() {
-    console.log('onFocus');
-  },
-  handleBlur() {
-    console.log('onBlur');
-  },
+  handleFocus() {},
+  handleBlur() {},
   handleCancel() {
     this.setData({
       value: '',
     });
-    console.log('onCancel');
   },
   handleSubmit(value) {
     my.alert({
       content: value,
     });
-    console.log(value, 'onSubmit');
   },
 });
