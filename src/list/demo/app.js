f4a4c425afdbf74806be33498b5f4e38d5145bb8
pingan1927 <pengsencai1986@gmail.com>
2018-04-24 00:04:18 +0800

    up

--- src/list/demo/app.js
+++ src/list/demo/app.js
@@ -1,3 +1,67 @@
 
 Page({
+  data: {
+    components: [
+      {
+        name: 'List',
+        nameZN: '列表',
+        path: '/list/demo/index',
+      },
+      {
+        name: 'InputItem',
+        nameZN: '列表输入项',
+        path: '/input-item/demo/index',
+      },
+      {
+        name: 'Modal',
+        nameZN: 'Modal',
+        path: '/modal/demo/index',
+      },
+      {
+        name: 'SwipeAction',
+        nameZN: '滑动删除',
+        path: '/swipe-action/demo/index',
+      },
+      {
+        name: 'Tabs',
+        nameZN: 'Tabs',
+        path: '/tabs/demo/index',
+      },
+      {
+        name: 'Stepper',
+        nameZN: 'Stepper',
+        path: '/stepper/demo/index',
+      },
+      {
+        name: 'Grid',
+        nameZN: '宫格',
+        path: '/grid/demo/index',
+      },
+      {
+        name: 'Card',
+        nameZN: '卡片',
+        path: '/card/demo/index',
+      },
+      {
+        name: 'Filter',
+        nameZN: '筛选',
+        path: '/filter/demo/index',
+      },
+      {
+        name: 'Notice',
+        nameZN: '公告',
+        path: '/notice/demo/index',
+      },
+      {
+        name: 'SearchBar',
+        nameZN: '搜索框',
+        path: '/search-bar/demo/index',
+      },
+      {
+        name: 'Message',
+        nameZN: '信息状态',
+        path: '/message/demo/index',
+      }
+    ],
+  }
 });
