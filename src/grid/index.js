5290fd30d9801db032f4c6162cf8fca9f11d86ff
Phieo <shikong2014@gmail.com>
2018-06-11 10:53:20 +0800

    fix: fix UI

--- src/grid/index.js
+++ src/grid/index.js
@@ -1,6 +1,6 @@
 Component({
   data: {
-    bottomIndex: 0, 
+    bottomIndex: 0,
   },
   props: {
     columnNum: 3,
@@ -16,13 +16,13 @@ Component({
       bottomIndex: Math.floor(rows) === rows ? (rows - 1) * columnNum : Math.floor(rows) * columnNum,
     });
   },
-  methods:{
-    onGridItemClick(e){
+  methods: {
+    onGridItemClick(e) {
       this.props.onGridItemClick({
         detail: {
-          index: e.target.dataset.index
-        }
+          index: e.target.dataset.index,
+        },
       });
     },
   },
-})
+});
