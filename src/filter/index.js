4e9a07ad6631a36925c4f7c1d7f78693d023baae
Phieo <shikong2014@gmail.com>
2018-09-26 14:29:26 +0800

    fix: 修复filter组件单选时要反选的bug

--- src/filter/index.js
+++ src/filter/index.js
@@ -4,39 +4,31 @@ Component({
   mixins: [lifecycle],
   data: {
     maxHeight: 0,
-    singleSelect: true
   },
   props: {
     className: '',
     onChange: () => {},
-    max: 10000
+    max: 10000,
   },
   didMount() {
-    let { commonProps, singleSelect } = this.data;
-    let { max } = this.props;
+    const { commonProps } = this.data;
+    const { max } = this.props;
     commonProps.max = max;
-    if (commonProps.max === 1) {
-      singleSelect = false;
-      this.setData({
-        singleSelect
-      });
-    }
-
   },
   methods: {
     resetFn() {
-      let { items, results } = this.data;
-      items.forEach(element => {
+      const { items, results } = this.data;
+      items.forEach((element) => {
         element.setData({
-          confirmStyle: ''
-        })
+          confirmStyle: '',
+        });
       });
       results.splice(0, results.length);
     },
     confirmFn() {
       const { onChange } = this.props;
-      let { results } = this.data;
+      const { results } = this.data;
       onChange(results);
     },
-  }
-})
+  },
+});
