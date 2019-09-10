932d485c1132191870c18de6558ad509bab2a793
linxz <linxz@vip.qq.com>
2019-09-10 10:27:50 +0800

    fix: 使用 component2 编译时 List 组件循环报错修复

--- components/list/index.ts
+++ components/list/index.ts
@@ -23,8 +23,10 @@ Component({
   didUpdate() {
     const loadTxt = this.props.loadContent[0] ? this.props.loadContent[0] : this.data.loadContent[0];
     const overTxt = this.props.loadContent[1] ? this.props.loadContent[1] : this.data.loadContent[1];
-    this.setData({
-      loadContent: [loadTxt, overTxt],
-    });
+    if (loadTxt !== this.data.loadContent[0] || overTxt !== this.data.loadContent[1]) {
+      this.setData({
+        loadContent: [loadTxt, overTxt],
+      });
+    }
   },
 });
